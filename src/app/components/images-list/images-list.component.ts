793ecec1f5e285fc0a196ccd0a863b98846d1a56
Mario <mariohertu@gmail.com>
2025-02-07 09:03:33 -0600

    Feature: Add Gathering of repositories

--- src/app/components/images-list/images-list.component.ts
+++ src/app/components/images-list/images-list.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import {MatListModule} from '@angular/material/list'
+import { ImagesService } from '../../services/images.service';
 
 @Component({
   selector: 'app-images-list',
@@ -11,5 +12,23 @@ import {MatListModule} from '@angular/material/list'
   styleUrl: './images-list.component.css'
 })
 export class ImagesListComponent {
+
+  imService = inject(ImagesService);
+  repositories = [];
+
+  constructor() {
+    this.getAllImages();
+  }
+
+  getAllImages(){
+    this.imService.getAllImages().subscribe({
+      next: (data: any) => {
+        this.repositories = data['repositories'];
+      },
+      error: (err: any) => {
+        this.repositories = [];
+      }
+    }); 
+  }
 
 }
--- src/app/services/images.service.ts
+++ src/app/services/images.service.ts
@@ -1,6 +1,6 @@
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +13,12 @@ export class ImagesService {
   constructor() { }
 
   getAllImages() {
-    return this.http.get(`${this.base_url}/v2/_catalog`);
+    // const images_url = this.base_url + "/v2/_catalog";
+    const images_url = "http://localhost:4200/v2/_catalog";
+    return this.http.get(images_url,
+      {headers: {
+        "Access-Control-Allow-Origin": images_url
+      }}
+    );
   }
 }
