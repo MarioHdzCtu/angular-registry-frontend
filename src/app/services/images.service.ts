0326d25ac1318ed07ce803f2db823a9d84dcfd3f
Mario <mariohertu@gmail.com>
2025-02-07 23:15:07 -0600

    Added endpoints for getting image tags and image manifest to service

--- src/app/services/images.service.ts
+++ src/app/services/images.service.ts
@@ -7,18 +7,32 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class ImagesService {
 
-  private base_url = environment.url;
+  private base_url = "http://localhost:4200";
   http = inject(HttpClient);
 
   constructor() { }
 
   getAllImages() {
     // const images_url = this.base_url + "/v2/_catalog";
-    const images_url = "http://localhost:4200/v2/_catalog";
+    const images_url = `${this.base_url}/v2/_catalog`;
     return this.http.get(images_url,
       {headers: {
         "Access-Control-Allow-Origin": images_url
       }}
     );
+  }
+
+  getImageTags(image: string){
+    const image_tags_url = `${this.base_url}/v2/${image}/tags/list`;
+    return this.http.get(image_tags_url);
+  }
+
+  getImageManifest(image: string, tag: string){
+    const image_manifest_url = `${this.base_url}/v2/${image}/manifests/${tag}`
+    return this.http.get(image_manifest_url, {
+      headers: {
+        'Accept':'application/vnd.oci.image.index.v1+json'
+      }
+    });
   }
 }
