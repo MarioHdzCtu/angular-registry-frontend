230e3c6725f7d33244c44f9d035a63485b7ba6a8
Mario <mariohertu@gmail.com>
2025-02-07 23:31:05 -0600

    Add logic for getting images list and its tags.

--- src/app/components/images-list/images-list.component.ts
+++ src/app/components/images-list/images-list.component.ts
@@ -6,6 +6,11 @@ import { MatDividerModule } from '@angular/material/divider';
 import { RouterLink } from '@angular/router';
 import {TooltipPosition, MatTooltipModule} from '@angular/material/tooltip';
 
+interface repoTags {
+  name: string
+  tags: string[]
+}
+
 @Component({
   selector: 'app-images-list',
   standalone: true,
@@ -22,21 +27,42 @@ import {TooltipPosition, MatTooltipModule} from '@angular/material/tooltip';
 export class ImagesListComponent {
 
   imService = inject(ImagesService);
-  repositories = [];
+  repositories: string[] = [];
+  repositories_tags: repoTags[] = [];
 
   constructor() {
     this.getAllImages();
+
   }
 
   getAllImages(){
     this.imService.getAllImages().subscribe({
       next: (data: any) => {
         this.repositories = data['repositories'];
+        console.log(this.repositories);
+        this.getImagesTags();
       },
       error: (err: any) => {
         this.repositories = [];
       }
     }); 
+  }
+
+  getImagesTags(){
+    this.repositories.forEach(
+      (element) => {
+      this.imService.getImageTags(element).subscribe({
+        next: (data: any) => {
+          this.repositories_tags.push({
+            "name":data['name'],
+            tags: data['tags']
+          })
+        },
+        error: (err: any) => {
+          console.error(err);
+        }
+      });
+    });
   }
 
 }
