8fd8f0b0bb8eae29dca6ea966893307d5537cd30
Mario <mariohertu@gmail.com>
2025-02-07 21:57:38 -0600

    Add image details component to routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { ImagesListComponent } from './components/images-list/images-list.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { ImageDetailsComponent } from './components/image-details/image-details.component';
 
 export const routes: Routes = [
     {path: 'all', component:ImagesListComponent},
+    {path:'details/:image', component:ImageDetailsComponent},
     {path: '**', component: PageNotFoundComponent}
 ];
