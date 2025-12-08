007601954c64e13b6b3712fad093ab46a8627261
MarioHdzCtu <mariohertu@gmail.com>
2025-12-07 21:26:45 -0600

    add home component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { ImagesListComponent } from './components/images-list/images-list.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { ImageDetailsComponent } from './components/image-details/image-details.component';
+import { HomeComponentComponent } from './components/home-component/home-component.component';
 
 export const routes: Routes = [
+    {path: '', component: HomeComponentComponent},
     {path: 'all', component:ImagesListComponent},
     {path:'details/:image', component:ImageDetailsComponent},
     {path: '**', component: PageNotFoundComponent}
